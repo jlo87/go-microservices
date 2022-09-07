c58c9f052c37e2a39fe664cc75a20f9d2ba915d3
Jonathan Lo <jonlo1987@gmail.com>
2022-09-06 23:56:14 -0400

    Added middlewarevalidation

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/jlo87/go-microservices/handlers"
 	"github.com/nicholasjackson/env"
 )
@@ -17,49 +18,59 @@ var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for t
 func main() {
 
 	env.Parse()
-	// logger
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	// Create references to the hello and goodbye handlers
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+
+	// create the handlers
 	ph := handlers.NewProducts(l)
 
-	// Create a new servemux
-	sm := http.NewServeMux()
+	// create a new serve mux and register the handlers
+	sm := mux.NewRouter()
 
-	// Register the endpoints
-	sm.Handle("/", ph)
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
 
-	// Create a new server
-	s := &http.Server{
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
+
+	//sm.Handle("/products", ph)
+
+	// create a new server
+	s := http.Server{
 		Addr:         *bindAddress,      // configure the bind address
 		Handler:      sm,                // set the default handler
 		ErrorLog:     l,                 // set the logger for the server
-		IdleTimeout:  120 * time.Second, // max time to for connections using TCP Keep-Alive
-		ReadTimeout:  1 * time.Second,   // max time to read request from the cient
-		WriteTimeout: 1 * time.Second,   // max time to write request to the client
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
-	// Handle ListenAndServe in a go func to prevent blocking
+
+	// start the server
 	go func() {
 		l.Println("Starting server on port 9090")
 
 		err := s.ListenAndServe()
 		if err != nil {
-			l.Fatal(err)
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
 		}
-
 	}()
 
-	// Broadcast message on this channel whenever an os.Kill/Interrupt is recieved
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, os.Interrupt)
-	signal.Notify(sigChan, os.Kill)
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
 
-	// Block here; reading from channel will block until there
-	// is a message to be consumed
-	sig := <-sigChan
-	l.Println("Received terminate, graceful shutdown", sig)
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
 
-	// Wait until requests handled by the server have completed, then shutdown
-	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	s.Shutdown(tc)
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
